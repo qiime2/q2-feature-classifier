512e8da45c442a9a1408c776a24b30d07f157e80
Matthew Dillon <matthewrdillon@gmail.com>
2016-11-07 15:12:21 -0700

    MAINT: Bump version to 0.0.7.dev0 (#38)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="q2-feature-classifier",
-    version='0.0.6.dev',
+    version='0.0.7.dev0',
     packages=find_packages(),
     install_requires=['qiime >= 2.0.6', 'q2-types >= 0.0.6', 'scikit-bio',
                       'biom-format >= 2.1.5, < 2.2.0', 'scikit-learn'],
