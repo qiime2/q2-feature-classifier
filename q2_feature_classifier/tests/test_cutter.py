5cf1d6a8a91421231923a48b04def7866afe4c40
BenKaehler <kaehler@gmail.com>
2017-05-18 23:29:24 +1000

    ENH: Add 5' trimming to extract-reads (#69)
    Fixes #65

--- q2_feature_classifier/tests/test_cutter.py
+++ q2_feature_classifier/tests/test_cutter.py
@@ -6,6 +6,10 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import os
+from itertools import product, islice
+
+import skbio
 from qiime2.plugins import feature_classifier
 from qiime2.sdk import Artifact
 from q2_types.feature_data import FeatureData, Sequence, DNAIterator
@@ -18,21 +22,43 @@ class CutterTests(FeatureClassifierTestPluginBase):
 
     def setUp(self):
         super().setUp()
-        self.sequences = Artifact.import_data(
-            'FeatureData[Sequence]', self.get_data_path('dna-sequences.fasta'))
+        seqs = skbio.io.read(self.get_data_path('dna-sequences.fasta'),
+                             format='fasta', constructor=skbio.DNA)
+        tmpseqs = os.path.join(self.temp_dir.name, 'temp-seqs.fasta')
+        skbio.io.write((s for s in islice(seqs, 10)), 'fasta', tmpseqs)
+        self.sequences = Artifact.import_data('FeatureData[Sequence]', tmpseqs)
 
     def test_extract_reads(self):
         # extract_reads should generate a FeatureData(Sequence) full of reads
-        length = 75
         f_primer = 'AGAGTTTGATCMTGGCTCAG'
         r_primer = 'GCTGCCTCCCGTAGGAGT'
         extract_reads = feature_classifier.methods.extract_reads
-        result = extract_reads(self.sequences, f_primer=f_primer,
-                               r_primer=r_primer, length=length, identity=0.9)
-        self.assertEqual(result.reads.type, FeatureData[Sequence])
         inseqs = list(self.sequences.view(DNAIterator))
-        outseqs = list(result.reads.view(DNAIterator))
-        self.assertLessEqual(len(outseqs), len(inseqs))
-        self.assertTrue(bool(outseqs))
-        for seq in outseqs:
-            self.assertEqual(len(seq), length)
+
+        trunc_lens = 0, 75, 5
+        trim_lefts = 0, 5
+        raw_lens = []
+        for trunc_len, trim_left in product(trunc_lens, trim_lefts):
+            if trunc_len == trim_left and trunc_len > 0:
+                with self.assertRaisesRegex(RuntimeError, "No matches found"):
+                    result = extract_reads(
+                        self.sequences, f_primer=f_primer, r_primer=r_primer,
+                        trunc_len=trunc_len, trim_left=trim_left, identity=0.9)
+                continue
+            result = extract_reads(
+                self.sequences, f_primer=f_primer, r_primer=r_primer,
+                trunc_len=trunc_len, trim_left=trim_left, identity=0.9)
+            self.assertEqual(result.reads.type, FeatureData[Sequence])
+            outseqs = list(result.reads.view(DNAIterator))
+            self.assertGreater(len(outseqs), 0)
+            self.assertLessEqual(len(outseqs), len(inseqs))
+            self.assertTrue(bool(outseqs))
+            if trunc_len != 0:
+                for seq in outseqs:
+                    self.assertEqual(len(seq), trunc_len - trim_left)
+            elif trim_left == 0:
+                for seq in outseqs:
+                    raw_lens.append(len(seq))
+            else:
+                for seq, raw_len in zip(outseqs, raw_lens):
+                    self.assertEqual(len(seq), raw_len - trim_left)
