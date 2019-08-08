ebb4c6400746c86210f65b1c8a0c216253a2879f
Anthony <oddant1@hotmail.com>
2019-08-08 09:54:56 -0700

    SQUASH: Addressing @nbokulich's review

--- q2_feature_classifier/_cutter.py
+++ q2_feature_classifier/_cutter.py
@@ -168,10 +168,12 @@ def extract_reads(sequences: DNASequencesDirectoryFormat, f_primer: str,
     q2_types.DNAFASTAFormat
         containing the reads
     """
-    if min_length > trunc_len and trunc_len > 0:
+    if min_length >= trunc_len and trunc_len > 0:
         raise ValueError('Your minimum length is larger than your truncation '
-                         'length, this will result in all of your sequences '
-                         'being trimmed automatically.')
+                         'length. This will result in all of your sequences '
+                         'being removed from your dataset. To procees, you '
+                         'must set a minimum length less than your truncation '
+                         'length.')
     n_jobs = effective_n_jobs(n_jobs)
     if batch_size == 'auto':
         batch_size = _autotune_reads_per_batch(
